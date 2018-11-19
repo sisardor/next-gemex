d3786fbc4f53463b34de77e12afce91578d04449
Sardor Isakov <sisardor@gmail.com>
2018-11-18 21:25:43 -0800

    added babelrc

---
+++ src/global-styles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+/* eslint no-unused-expressions: 0 */
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    min-height:100%;
+    position:relative;
+    padding-bottom:100px;
+    overflow: hidden;
+    background: #e3e8ee;
+  }
+`;
+export default GlobalStyle;
