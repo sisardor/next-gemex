d43140cb6741c02148e209775752fa9d6b2c906f
Sardor Isakov <sisardor@gmail.com>
2019-03-03 20:31:53 -0800

    update

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,22 +1,43 @@
-import {withRouter} from 'next/router'
+import redirect from '../lib/redirect'
 import Layout from '../components/MyLayout'
 import fetch from 'isomorphic-unfetch'
 import { NextAuth } from 'next-auth/client'
 
-const Admin = (props) => {
-  return (<Layout>
-    Admin
-  </Layout>);
-}
 
+export default class Admin extends React.Component {
+  static async getInitialProps({ ctx, req }) {
 
-Admin.getInitialProps = async function ({ req }) {
-  const session =  await NextAuth.init({req});
-  console.log(session);
-  return {
-      session: session,//await NextAuth.init({req}),// Add this.props.session to all pages
-      // lang: 'en' // Add a lang property to all pages for accessibility
+    const session =  await NextAuth.init({req});
+    const providers = await NextAuth.providers({req})
+    console.log('providers', providers);
+    if (!session.user) {
+      // Already signed in? No need to continue.
+      // Throw them back to the main page
+      redirect(ctx, '/')
+      return {}
     }
-}
+    console.log(session);
+    return {
+      session,// await NextAuth.init({req}),// Add this.props.session to all pages
+      lang: 'en', // Add a lang property to all pages for accessibility
+      providers
+    }
+  }
 
-export default Admin;
+  adminAccessOnly() {
+    return (
+      <div className="text-center pt-5 pb-5">
+        <h1 className="display-4 mb-5">Access Denied</h1>
+        <p className="lead">You must be signed in as an administrator to access this page.</p>
+      </div>
+    )
+  }
+
+  render() {
+    console.log(this.props);
+    if (!this.props.session.user || this.props.session.user.admin !== true)
+      return this.adminAccessOnly()
+    return <p>Admin</p>
+  }
+
+}
